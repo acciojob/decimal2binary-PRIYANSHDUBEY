b6ba7561cd07b268a918e18cd15226047676fe15
PRIYANSHDUBEY <87897527+PRIYANSHDUBEY@users.noreply.github.com>
2023-09-18 02:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  if (num === 0) {
+    return "0"; // Special case for decimal 0
+  }
+
+  let binary = "";
+  while (num > 0) {
+    // Append the remainder of num divided by 2 to the binary string
+    binary = (num % 2) + binary;
+
+    // Integer division by 2 to update num
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
-window.decimalToBinary = decimalToBinary;
+// Example usage:
+const decimalNumber = 10; // Change this to the decimal number you want to convert
+const binaryNumber = decimalToBinary(decimalNumber);
+console.log(binaryNumber);
